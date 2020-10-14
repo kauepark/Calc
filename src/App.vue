dbca58cd40ab01ff4f04e933825dc0d3d73e2d48
kauepark <kauemalicious@outlook.com>
2020-10-14 03:09:27 -0300

    Atividade VII de LPW

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <input id="val1" type="text" placeholder="Digite um valor">
+    <hr>
+    <input id="val2" type="text" placeholder="Digite outro valor">
+    <hr>
+    <button v-on:click="somar">Somar</button>
+    <div>
+      <h1 id="res">{{resultado}}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      resultado: ''
+    };
+  },
+  methods: {
+    somar: function() {
+      this.resultado = parseInt(document.getElementById("val1").value) + parseInt(document.getElementById("val2").value)
+      document.querySelector("res").innerHTML = this.resultado
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
